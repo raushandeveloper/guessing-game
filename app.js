c78cfcd8f9c81d14d7ca982ab32f6ebcb22e3faf
Raushan Kumar Singh <raushanrajput7033@gmail.com>
2025-06-16 13:45:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const max = prompt("Enter the max number");
+const random = Math.floor(Math.random() * max)+1;
+console.log(random);
+
+let guess = prompt("guess the number");
+
+while(true){
+    if(guess=="quit"){
+        console.log("user quit");
+        break;
+    }
+
+    if(guess== random ){
+        console.log("you are right! congrats!! random number was",random);
+        break;
+    }else if(guess<random){
+        guess = prompt("hint: your guess was too small. Please try again");
+    } else {
+        guess = prompt("hint: your guess was too large. please try again");
+    }
+    //  else{
+    //    guess = prompt("your guess was wrong. please try again");
+    // }
+
+}
